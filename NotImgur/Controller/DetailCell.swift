e5fc47669b21dac1392574e8195697d0e7d0218a
Quy33 <87293639+Quy33@users.noreply.github.com>
2021-11-15 09:09:56 +0700

    Fixed the image not expanding because lower content hugging priorities == expansion , higher equal original intrinsic size

--- NotImgur/Controller/DetailCell.swift
+++ NotImgur/Controller/DetailCell.swift
@@ -12,7 +12,11 @@ class DetailCell: UITableViewCell {
     @IBOutlet weak var cellImage: UIImageView?
     @IBOutlet weak var titleLabel: UILabel?
     @IBOutlet weak var descriptionLabel: UILabel?
-//    @IBOutlet weak var stackView: UIStackView?
+    @IBOutlet weak var topLabel: UILabel?
+    @IBOutlet weak var bottomLabel: UILabel?
+    @IBOutlet weak var outerView: UIView?
+    @IBOutlet weak var upperStackView: UIStackView?
+    @IBOutlet weak var lowerStackView: UIStackView?
 //
 //    static let identifier = "DetailCell"
 //    var stackWidth : CGFloat {
@@ -55,9 +59,12 @@ class DetailCell: UITableViewCell {
     
     static let identifier = "DetailCell"
     
-    func config(image: UIImage, title: String?, desc: String?){
+    func config(image: UIImage, title: String?, desc: String?, top: String?, bottom: String?){
         cellImage?.image = image
         titleLabel?.text = title
         descriptionLabel?.text = desc
+        topLabel?.text = top
+        bottomLabel?.text = bottom
+        bottomLabel?.isHidden = true
     }
 }
--- NotImgur/Controller/RedoDetailTableView.swift
+++ NotImgur/Controller/RedoDetailTableView.swift
@@ -14,6 +14,8 @@ class RedoDetailTableView: UITableViewController {
     var itemGot = (id: "KxiXTUT",isAlbum: true)
     
     private var imgurManager = ImgurNetworkManager()
+    private var isCached = false
+    private var heights: [CGFloat] = []
     
     private let dummyTitle = """
     "Every betrayal contains a perfect moment, a coin stamped heads or tails with salvation on the other side."
@@ -36,27 +38,26 @@ class RedoDetailTableView: UITableViewController {
     var dummyDesc5 = """
     “I’m not getting in the car, Tim,” Mr Pence replied. “I trust you, Tim, but you’re not driving the car. If I get in that vehicle, you guys are taking off. I’m not getting in the car.”\n\n-Mike Pence, January 6th, 2021\n\nWhat if they weren\'t chanting: \"Hang Mike Pence!\" but were actually chanting: \"This Great Fence!\" or \"This Makes Sense!\" or something?  I\'m going head on over to the fOxNEwS and see what the Branson set thinks.
     """
-    private var dummy = AlbumDetailItem(title: "Something", description: nil, images: .init(repeating: ImageDetailItem(), count: 20))
-
-
+    private var dummy = AlbumDetailItem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         registerCell(DetailCell.identifier)
-        registerCell(TopAndBottomCell.identifier)
         
-        tableView.rowHeight = 40
-        
-        
+        //Top
         Task {
             let model = try await imgurManager.getDetail(with: itemGot)
-            print(model.data.title)
-            print(model.data.description)
-            print(model.data.images?.count)
-            for item in model.data.images! {
-                print("title: \(item.title)")
-                print("desc: \(item.description)")
+            dummy = getAlbumDetail(model)
+            let urls = dummy.images.compactMap{ $0.url }
+            let images = try await imgurManager.multipleDownload(with: urls)
+
+            for (index,item) in dummy.images.enumerated() {
+                item.image = images[index]
             }
+
+            heights = .init(repeating: 0.0, count: dummy.images.count)
+            tableView.reloadData()
         }
     }
 
@@ -68,38 +69,118 @@ class RedoDetailTableView: UITableViewController {
         guard !dummy.images.isEmpty else {
             return 0
         }
-        return dummy.images.count + 2
+        return dummy.images.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: DetailCell.identifier, for: indexPath) as! DetailCell
         
-        let labelCell = tableView.dequeueReusableCell(withIdentifier: TopAndBottomCell.identifier, for: indexPath) as! TopAndBottomCell
+//        let placeHolder = UIImage(named: "placeholder")!
+        //let placeHolder = #imageLiteral(resourceName: "First(1)")
         
-        let placeHolder = UIImage(named: "placeholder")!
-        // Configure the cell...
-        //Top
-        if indexPath.row == 0 {
-            return labelCell
-        } else if indexPath.row == dummy.images.count {
-            return labelCell
-        } else {
-            //The rest
-            guard !dummy.images.isEmpty else {
-                return cell
-            }
-            let imagesItem = dummy.images[indexPath.row]
-            cell.config(image: placeHolder, title: imagesItem.title, desc: imagesItem.description)
+        guard !dummy.images.isEmpty else {
             return cell
         }
+        // Configure the cell...
+        
+        let item = dummy.images[indexPath.row]
+        
+        if indexPath.row == 0 {
+            //Top
+            cell.config(image: item.image, title: item.title, desc: item.description, top: dummy.title, bottom: nil)
+        }else if indexPath.row == dummy.images.count - 1 {
+            //Bottom
+            cell.config(image: item.image, title: item.title, desc: item.description, top: nil, bottom: dummy.description)
+        } else {
+            cell.config(image: item.image, title: item.title, desc: item.description, top: nil, bottom: nil)
+        }
+        
+        return cell
     }
 
     //MARK: Table View Delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard !dummy.images.isEmpty else {
+            return 0
+        }
+        return heights[indexPath.row]
+    }
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let detailCell = cell as? DetailCell else{
+            return
+        }
+        //let labelHeight = detailCell.topLabel?.frame.height
+        if !heights.isEmpty {
+            if !isCached {
+                let frameWidth = detailCell.outerView!.frame.width
+                for (index,item) in dummy.images.enumerated() {
+
+                    let titleHeight = heightForView(text: item.title ?? "", font: .systemFont(ofSize: 17), width: frameWidth)
+                    let descHeight = heightForView(text: item.description ?? "", font: .systemFont(ofSize: 17), width: frameWidth)
+                    let imageHeight = calculateHeight(item.image.size)
+
+                    switch index {
+                    case 0:
+                        let topTitleHeight = heightForView(text: dummy.title, font: .systemFont(ofSize: 17), width: frameWidth)
+                        heights[index] = topTitleHeight + titleHeight + descHeight + imageHeight
+                    case dummy.images.count - 1:
+                        let bottomDescHeight = heightForView(text: dummy.description ?? "", font: .systemFont(ofSize: 17), width: frameWidth)
+
+                        heights[index] = bottomDescHeight + titleHeight + descHeight + imageHeight
+                    default:
+                        heights[index] = titleHeight + descHeight + imageHeight
+                    }
+                }
+                isCached = true
+                print(heights)
+                tableView.reloadData()
+            }
+        }
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! DetailCell
+        print(cell.topLabel?.frame.height)
+        print(cell.titleLabel?.frame.height)
+        print(cell.cellImage?.frame.height)
+        print(cell.descriptionLabel?.frame.height)
+        print(cell.bottomLabel?.frame.height)
+        print(cell.frame.height)
+        print(cell.bottomLabel?.text)
+        print("...")
+        print(calculateHeight(dummy.images[indexPath.row].image.size))
+    }
     
     //MARK: Misc Functions
     func registerCell(_ identifier: String) {
         let nib = UINib(nibName: identifier, bundle: nil)
         tableView.register(nib , forCellReuseIdentifier: identifier)
+    }
+    func calculateHeight(_ pictureSize: CGSize)->CGFloat{
+        let deviceSize = view.frame.size
+        let wOffSet = pictureSize.width - deviceSize.width
+        let wOffSetPercent = (wOffSet*100)/pictureSize.width
+        let hOffSet = (wOffSetPercent*pictureSize.height)/100
+        let newHeight = pictureSize.height - hOffSet
+        return newHeight
+    }
+    func heightForView(text: String, font: UIFont, width: CGFloat) -> CGFloat {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = .byTruncatingTail
+        label.font = font
+        label.text = text
+
+        label.sizeToFit()
+        return label.frame.height
+    }
+    private func getAlbumDetail(_ model: DetailModel) -> AlbumDetailItem {
+        var newAlbum = AlbumDetailItem(title: model.data.title, description: model.data.description, images: [])
+        let items = model.data.images!
+        for image in items {
+            let newImage = ImageDetailItem(title: image.title, description: image.description, link: image.link, animated: image.animated, mp4: image.mp4)
+            newAlbum.images.append(newImage)
+        }
+        return newAlbum
     }
 }
